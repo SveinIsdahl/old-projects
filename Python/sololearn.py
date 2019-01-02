d47c90caf6f550977aa29a480ea984bbc167945f
BuildTools <unconfigured@null.spigotmc.org>
2019-01-02 20:39:40 +0100

    py

--- Python/sololearn.py
+++ Python/sololearn.py
@@ -1,2 +1,13 @@
 #print(float(input("Skriv inn et tall: \n"))+
 #float(input("Skriv inn et til får å kunne legge dem sammen: \n")))
+x="a"
+x*=3
+print(x)
+print(7!=8)
+print(7==7.0)
+print(7=="7")
+x=1
+if 2 < x:
+    print("2<" + str(x))
+else:
+    print("2>" + str(x))
