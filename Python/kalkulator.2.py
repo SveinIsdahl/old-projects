b6b2d21d00fb747a2be1f728cc81350058bee888
Svein Isdahl <svein@Sveins-MacBook-Pro.local>
2019-01-06 13:09:28 +0100

    mac edits

---
+++ Python/kalkulator.2.py
@@ -0,0 +1,7 @@
+x=1
+while True:
+    print("Skriv inn to et tall")
+    print("Skriv så inn et til")
+    num1 = float((raw_input("Skriv inn et tall")))
+    num2 = float((raw_input("Og et til")))
+    print((num1) + (num2)
