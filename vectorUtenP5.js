df1795d4b6b86ced3f84ac5d8a211bafbc136c6f
BuildTools <unconfigured@null.spigotmc.org>
2019-02-16 21:47:01 +0100

    Alt virker, ball skifter farge når treffer vegg

--- vectorUtenP5.js
+++ vectorUtenP5.js
@@ -2,46 +2,66 @@ let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
 let radius = 40
-let gravity = 0.5;
-let bounce = 0.1;
 
 let pos = {x:radius+700, y:radius};
 let vel = {x:0, y:0};
-let acc = {x:0.00, y:0.00};
+let acc = {x:0.000, y:0.00};
+
+function ball(){
+    ctx.lineWidth = "5";
+    ctx.beginPath();
+    ctx.ellipse(pos.x, pos.y, radius, radius, 2 * Math.PI, 0, 2 * Math.PI);
+
+    let ran1 = Math.floor(Math.random() * 255) + 0;
+    let ran2 = Math.floor(Math.random() * 255) + 0;
+    let ran3 = Math.floor(Math.random() * 255) + 0;
+    console.log(ran1)
+    if (pos.x > 1775 || pos.x < 25) {    
+        ctx.fillStyle = `rgb(
+        ${Math.floor(ran1)},
+        ${Math.floor(ran2)}, 
+        ${Math.floor(ran3)})`;
+    }
+    if (pos.y > 870 || pos.y < 25) {
+        ctx.fillStyle = `rgb(
+        ${Math.floor(ran3)},
+        ${Math.floor(ran1)}, 
+        ${Math.floor(ran2)})`;
+
+    }
+    ctx.fill();
+    ctx.stroke();
+}
 
 function draw() {
-
-    if (vel.x > 1000){
-        vel.x = 5;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ball();
+    if (pos.y < 0){
+        pos.y = 5;
     }
-    if (vel.y > 1000) {
-        vel.y = 5;
+    if (pos.x < 0) {
+        pos.x = 5;
     }
-
-    acc.y += gravity;
-
 
     pos.x += vel.x;
     pos.y += vel.y;
 
     vel.x += acc.x;
     vel.y += acc.y;  
-      
-    if (pos.x > 1775){
-        pos.x = radius;
+    
+    if (pos.x > 1775 || pos.x < 25){
+        vel.x = -vel.x;  
     }
-    if (pos.y > 870) {
-        vel.y *= -bounce;
+    if (pos.y > 870 || pos.y < 25) {
+        vel.y = -vel.y;
+
     }
-    ctx.lineWidth = "5";
-    ctx.beginPath();
-    ctx.ellipse(pos.x, pos.y, radius, radius, 2 * Math.PI, 0, 2 * Math.PI);
-    ctx.stroke();
+    
+    let ranY = (Math.random() - 0.5);
+    let ranX = (Math.random() - 0.5);
+    acc.y = ranY/10;
+    acc.x = ranX/10;
     
 }
-function drawLoop(e) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    draw();
-}
 
-var loop = setInterval(drawLoop, 10);
+setInterval(draw, 1.6);
