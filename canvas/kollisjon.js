b7f255c0d3ddb133c0356e181ef6f7b7caa60e40
Svein Isdahl <300708.svisdahl@gmail.com>
2019-02-27 19:59:38 +0100

    2 objekter, draw funksjon som property, ikke ferdig.

--- canvas/kollisjon.js
+++ canvas/kollisjon.js
@@ -8,7 +8,10 @@ let ball1 = {
     xvel: 0,
     yvel: 0,
     xacc: 0.0017,
-    yacc: 0.0032
+    yacc: 0.0032,
+    draw: function(){
+        this.
+    }
 }
 let ball2 = {
     r: 50,
@@ -21,7 +24,9 @@ let ball2 = {
 }
 
 function draw(){
-
+    
 }
+
+setInterval(draw, 16);
 // funskjon som tar en parameter i form av et objekt og bruker properties i objektet til å utføre funksjon
 // ball1 og ball 2 i en funksjon^ hvor alt som endres i fiunksjonen er objektet ball og dets properties
