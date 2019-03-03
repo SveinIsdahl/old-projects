701d583158e2576cf9d641ffecdf263c421f25e2
Svein Isdahl <300708.svisdahl@gmail.com>
2019-03-03 23:01:01 +0100

    Har nå funksjon som lager baller ut i fra objekter kalt "ball"

--- canvas/kollisjon.js
+++ canvas/kollisjon.js
@@ -1,29 +1,68 @@
 let canvas = document.getElementById("canvas");
-let ctx = document.getContext("2d");
+let ctx = canvas.getContext("2d");
+
+let width = canvas.width;
+let height = canvas.height;
 
 let ball1 = {
     r: 50,
-    xpos: 300,
-    ypos: 300,
+    x: 300,
+    y: 300,
     xvel: 0,
     yvel: 0,
-    xacc: 0.0017,
-    yacc: 0.0032,
+    xacc: 0.0007,
+    yacc: 0.032,
 }
 let ball2 = {
     r: 50,
-    xpos: 500,
-    ypos: 500,
+    x: 500,
+    y: 200,
     xvel: 0,
     yvel: 0,
-    xacc: 0.0038,
-    yacc: 0.0025
+    xacc: 0.008,
+    yacc: 0.075
 }
 
-function draw(){
-    
+let ball3 = {
+    r: 50,
+    x: 500,
+    y: 100,
+    xvel: 0,
+    yvel: 0,
+    xacc: 0.001,
+    yacc: 0.035
 }
 
+function ball(ball) {
+    ctx.lineWidth = "5";
+    ctx.beginPath();
+    ctx.ellipse(ball.x, ball.y, ball.r, ball.r, 2 * Math.PI, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+
+    ball.x += ball.xvel;
+    ball.y += ball.yvel;
+
+    ball.xvel += ball.xacc;
+    ball.yvel += ball.yacc;
+
+    if (ball.x > width - ball.r || ball.x < ball.r) {
+        ball.xvel *= -0.9;
+    }
+    if (ball.y > height - ball.r || ball.y < ball.r) {
+        ball.yvel *= -0.9;
+    }
+
+}
+
+function draw() {
+    ctx.clearRect(0, 0, width, height);
+
+    ball(ball1);
+    ball(ball2);
+    ball(ball3);
+}
 setInterval(draw, 16);
-// funskjon som tar en parameter i form av et objekt og bruker properties i objektet til å utføre funksjon
-// ball1 og ball 2 i en funksjon^ hvor alt som endres i fiunksjonen er objektet ball og dets properties
+
+//Legg til object constructor for å konstruere flere baller
+//Legg til collition-detection (Uten box2d, bruk tutorial på detection kun m. baller)
