0fb835f99a88329168c17d8a4fe86af4de2d186a
BuildTools <unconfigured@null.spigotmc.org>
2019-04-06 22:38:36 +0200

    Organiserte i mappe; "gammalt"

--- gammalt/linærregresjon.js
+++ gammalt/linærregresjon.js
@@ -4,8 +4,6 @@ let ys = [];
 function setup() {
     createCanvas(800, 600);
     background(0);
-    let m = tf.scalar(random(1)).variable();
-    let b = tf.scalar(random(1)).variable();
 
 }
 function mousePressed() {
