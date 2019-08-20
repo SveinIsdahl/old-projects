9a8821d10a815c743feddf60556c21db6f5ab805
Svein Isdahl <300708.svisdahl@gmail.com>
2019-08-20 17:26:21 +0200

    Restrukturering

---
+++ Sortering/bubble-farge.js
@@ -0,0 +1,6 @@
+// Bubble sorting Algorithm, bytter to variabler i et array hvis de ikke er i korekt rekkefølger, starter fra begynnelse/slutt
+let values = [];
+let a;
+let b;
+let i = 0;
+let j = 0;
