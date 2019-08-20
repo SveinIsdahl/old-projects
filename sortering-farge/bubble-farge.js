0e6d2153d88e67a43e5906190754b04a598afdfc
Svein Isdahl <300708.svisdahl@gmail.com>
2019-08-20 21:39:12 +0200

    Begynt på farge bubble sortering uten p5

--- sortering-farge/bubble-farge.js
+++ sortering-farge/bubble-farge.js
@@ -1,6 +1,3 @@
 // Bubble sorting Algorithm, bytter to variabler i et array hvis de ikke er i korekt rekkefølger, starter fra begynnelse/slutt
-let values = [];
-let a;
-let b;
-let i = 0;
-let j = 0;
+let verdier = [];
+
