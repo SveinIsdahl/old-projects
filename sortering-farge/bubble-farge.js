383f2d55ddb2fde086a9acf3b51f01db2fe92f3a
Svein Isdahl <300708.svisdahl@gmail.com>
2019-08-22 08:07:16 +0200

    Begynt på outline av funsjoner

--- sortering-farge/bubble-farge.js
+++ sortering-farge/bubble-farge.js
@@ -1,3 +1,23 @@
 // Bubble sorting Algorithm, bytter to variabler i et array hvis de ikke er i korekt rekkefølger, starter fra begynnelse/slutt
 let verdier = [];
 
+function setup() {
+    for (let i = 0; i < 1000; i++) {
+        verdier[i] = Math.floor(Math.random()*255);
+        
+    }
+}
+
+function tegn() {
+    
+    sorter();
+    function bytt() {
+        
+    }
+}
+
+function sorter() {
+    
+}
+setup();
+console.log(verdier);
