3d603bfc9ea1a532eda06fe865b3ebf5de80a24b
Svein Isdahl <300708.svisdahl@gmail.com>
2019-08-22 16:22:19 +0200

    Laget fargede linjer, ingen algo

--- sortering-farge/bubble-farge.js
+++ sortering-farge/bubble-farge.js
@@ -1,23 +1,35 @@
 // Bubble sorting Algorithm, bytter to variabler i et array hvis de ikke er i korekt rekkefølger, starter fra begynnelse/slutt
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
 let verdier = [];
 
 function setup() {
     for (let i = 0; i < 1000; i++) {
-        verdier[i] = Math.floor(Math.random()*255);
-        
+        verdier[i] = Math.floor(Math.random()*360);
     }
+    tegn();
 }
 
 function tegn() {
-    
+
+    for (let i = 0; i < 1000; i++) {
+        ctx.beginPath();
+        ctx.strokeStyle = "hsl("+verdier[i]+", 100%, 50%)";
+        ctx.moveTo(i,360);
+        ctx.lineTo(i,verdier[i]);
+        ctx.stroke();
+    }
     sorter();
+
+}
+
+function sorter() {
+
     function bytt() {
         
     }
 }
 
-function sorter() {
-    
-}
 setup();
 console.log(verdier);
