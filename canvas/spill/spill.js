c3c5cb71e3a40b5c3e3451f1763352a4ffa65487
Svein Isdahl <300708.svisdahl@gmail.com>
2019-09-09 22:06:10 +0200

    Har speed limit, endre til gradvis reduskjson?

--- canvas/spill/spill.js
+++ canvas/spill/spill.js
@@ -1,9 +1,77 @@
-
-
-function setup(){
-
+let max = 5;
+class template {
+    constructor(r, x, y, velX, velY, accX, accY) {
+        this.r = r;
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.accX = accX;
+        this.accY = accY;
+    }
 }
 
-function draw(){
-    
+function setup() {
+    createCanvas(windowWidth - 25, windowHeight - 25);
+    noCursor();
+    rect(0, 0, windowWidth-25, windowHeight-25);
 }
+
+let player = new template(30, 700, 600, 0, 0, 0, 0);
+
+function draw() {
+    clear();
+    fill(0,0,0,0);
+    rect(0, 0, windowWidth-26, windowHeight-26);
+    fill(100);
+    ellipse(player.x, player.y, player.r);
+
+    player.x += player.velX;
+    player.y += player.velY;
+
+    player.velX += player.accX;
+    player.velY += player.accY;
+
+    //S
+    if (keyIsDown(83)) {
+        player.velY += 0.2;
+    }
+    //D
+    if (keyIsDown(68)) {
+        player.velX += 0.2;
+    }
+    //W
+    if (keyIsDown(87)) {
+        player.velY -= 0.2;
+    }
+    //A
+    if (keyIsDown(65)) {
+        player.velX -= 0.2;
+    }
+
+    if (player.x > windowWidth-10-player.r || player.x < player.r/2){
+        player.velX = -player.velX;
+    }
+    if (player.y > windowHeight-10-player.r || player.y < player.r/2) {
+        player.velY = -player.velY;
+    }
+    if (player.velX > max){
+        player.velX = max;
+    }
+    if (player.velY > max){
+        player.velY = max;
+    }
+    if (player.velX < -max){
+        player.velX = -max;
+    }
+ 
+    if (player.velY < -max){
+        player.velY = -max;
+    }
+   console.log(player.velX);
+   console.log(player.velY)
+}
+
+
+// Bruke keyRelease til å 
+// øke/redusere vel ved å holde inne
