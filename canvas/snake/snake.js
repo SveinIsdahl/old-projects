598e9dd347610a45fac7419f82a8d364e092ebb6
SveinIsdahl <300708.svisdahl@gmail.com>
2019-12-26 01:34:36 +0100

    0

--- canvas/snake/snake.js
+++ canvas/snake/snake.js
@@ -3,19 +3,39 @@ const ctx = canvas.getContext("2d");
 const height = canvas.height;
 const width = canvas.width;
 
-
+let kjør = 1;
+let timer = 0;
 let theta = 0;
 let step = 2*Math.PI/100
 
 //hode
 let h = {
-    x:100,
-    y:100,
+    x:200,
+    y:200,
+    r:10,
+}
+
+let h2 = {
+    x:200,
+    y:200,
     r:10,
 }
 
 function draw() {
     ctx.clearRect(0, 0, width, height);
+    
+    ctx.lineWidth = "2";
+
+    timer += 1;    
+
+            
+    ctx.beginPath();    
+    ctx.arc(h2.x,h2.y, h2.r, h2.r, 0, Math.PI*2);
+    ctx.stroke();
+    if ((timer % 2) == 0) {
+        h2.x = h.x;
+        h2.y = h.y;
+    }
 
     if (theta < 2*Math.PI) {
         
@@ -30,12 +50,10 @@ function draw() {
     
 
     ctx.beginPath();
-    ctx.lineWidth = "2"
     ctx.arc(h.x,h.y, h.r, h.r, 0, Math.PI*2);
+    ctx.fillStyle = "white";
+    ctx.fill();
     ctx.stroke();
-
-    ctx.fillStyle = 'rgba(255, 255, 255, .05)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
   
     //Utenfor canvas = gameover
     if (((h.x + h.r) > width) || ((h.x - h.r) < 0)|| ((h.y + h.r) > height)|| ((h.y - h.r) < 0)) {
@@ -53,9 +71,19 @@ canvas.addEventListener("click", reverse);
 
 function gameover() {
     // stopp draw-loop
+    kjør = 0;
     console.log("Game Over");
 }
- 
 
+function run() {
+    if (kjør == 1) {
+        draw()
+    }
+}
+function start() {
+    kjør = 1;
+    h.x = 200;
+    h.y = 200;
+}
 
-setInterval(draw, 15);
+setInterval(run, 15);
