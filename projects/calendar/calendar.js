f3e78d1fe70b02115607939df4e3b13e0b4aacd2
Svein Isdahl <svein@Sveins-MacBook-Pro.local>
2020-09-27 12:54:53 +0200

    små endringer

--- projects/calendar/calendar.js
+++ projects/calendar/calendar.js
@@ -6,7 +6,7 @@ let stagedDataArray = [];
 let keys;
 
 function setup() {
-    // Your web app's Firebase configuration
+    // irebase configuration
     var firebaseConfig = {
         apiKey: "AIzaSyD_6EPzd56KtaDCm9k1Od-MorOcuxwMlMI",
         authDomain: "calendar-9d4d6.firebaseapp.com",
@@ -17,22 +17,13 @@ function setup() {
         appId: "1:103816515999:web:07a5093add3e224a14adaa"
     };
     // Initialize Firebase
-
     // @ts-ignore
     firebase.initializeApp(firebaseConfig);
-
     // @ts-ignore
     database = firebase.database();
     ref = database.ref("calendar");
-    //ref.push({ "date": "30.9", "text": "rangegdomness" })
 
     //On event value, 
-    /**
-     * @param {object} e
-     */
-    /**
-     * @param {string} e
-     */
     ref.on('value', gotData, (e) => { console.log("error " + e) })
 
     /**
@@ -47,14 +38,8 @@ function setup() {
             let k = keys[i];
             retrievedDataArray.push(data[k]);
         }
-        console.log("succesfully retrieved data");
-        console.log(retrievedDataArray);
         createCalendar();
-
     }
-
-
-
 
     /**
      * @param {Date} d
@@ -74,8 +59,6 @@ function setup() {
         return weekNo;
     }
 
-    //Array av ukene som er igjen av året, inneholder div-elementer
-    let ukeArray = [];
     // Array som inneholder ukene som er igjen av året, som igjen inneholder array for hver uke som inneholder 7 uke-div-elementer
     let dagUkeArray = [];
 
@@ -91,38 +74,26 @@ function setup() {
         //1-7
         let ukedag = date.getDay();
 
-        //1-31
-        // @ts-ignore
-        let dag = date.getDate();
-
-        //0-11
-        // @ts-ignore
-        let month = date.getMonth() + 0;
-
         dateAccumulator -= ukedag - 1;
-        for (let i = ukenr; i < 53; i++) {
-            let ukeDiv = ukeArray[i]
-
+        for (let i = (ukenr - 1); i < 53; i++) {
+            let ukeDiv;
             ukeDiv = document.createElement("div");
             kalender.append(ukeDiv);
             ukeDiv.className = "uke";
 
             dagUkeArray[i] = [];
-
             for (let j = 1; j <= 7; j++) {
                 let dagDiv = dagUkeArray[i][j];
 
                 let tempDate = new Date();
-                tempDate.setDate(new Date().getDate() + dateAccumulator);
+                tempDate.setDate(new Date().getDate() + dateAccumulator - 7);
 
                 let tempDateString = tempDate.getDate() + "." + (tempDate.getMonth() + 1);
-
 
                 dagDiv = document.createElement("textarea");
                 ukeDiv.append(dagDiv);
                 dagDiv.className = "dag";
                 dagDiv.style.resize = "none";
-
 
                 dagDiv.addEventListener("change", () => {
                     addToDataStaging(dagDiv, tempDateString);
@@ -134,7 +105,6 @@ function setup() {
                     let string = String(retrievedDataArray[k]["text"]);
 
                     //Dersom date i databasen === dagDiv sin dato, legg til tekst i dagDiv
-
                     if (retrievedDataArray[k]["date"] === tempDateString) {
                         if ((retrievedDataArray[k]["text"] == tempDateString) || string.includes(tempDateString) !== true || string.includes(tempDateString + "\n") !== true) {
                             dagDiv.value = tempDateString + "\r\n";
@@ -143,22 +113,20 @@ function setup() {
                         dagDiv.value = retrievedDataArray[k]["text"];
                         break;
                     }
-
                 }
-
-                if (dateAccumulator === 0) {
+                if (dateAccumulator === 7) {
                     dagDiv.style.background = "#99aaee"
                 }
-
-
-
                 dateAccumulator++
-
             }
         }
     }
 }
 
+/**
+ * @param {HTMLTextAreaElement} textarea
+ * @param {string} date
+ */
 function addToDataStaging(textarea, date) {
     const text = textarea.value;
     const stagingObject = {
