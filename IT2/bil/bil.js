e0409d4f7308156859e3320dfb70c8c009fb8769
Svein Isdahl <svein@Sveins-MacBook-Pro.local>
2020-10-15 08:37:39 +0200

    g

---
+++ IT2/bil/bil.js
@@ -0,0 +1,30 @@
+//@ts-check
+
+window.onload = () => {
+    const divMain = document.getElementById("main");
+    const btnSpill = document.querySelector("button");
+    const bil = document.getElementById("bil");
+    const bil2 = document.getElementById("bil2");
+
+
+    let lydfil = document.getElementById("lyd");
+    let krasj = document.getElementById("krasj")
+
+    btnSpill.addEventListener("click", () => {
+        //void btnSpill.offsetWidth;
+        lydfil.play();
+        setTimeout(() => { lydfil.play() }, 1000)
+        bil.classList.add("aktiv");
+        bil2.classList.add("aktiv");
+        setTimeout(() => {
+            //bil.classList.remove("aktiv");
+            //bil2.classList.remove("aktiv");
+            krasj.play();
+            bil2.classList.remove("aktiv");
+
+            bil2.classList.add("krasj");
+
+        }, 2600)
+    })
+
+}
--- IT2/oppsummering sept/sept.js
+++ IT2/oppsummering sept/sept.js
@@ -1,10 +1,30 @@
-//@ts-check
+// @ts-check
+
 window.onload = () => {
-    console.log("loaded");
     const divMain = document.getElementById("main");
-    divMain.innerHTML += "hei";
+    const divS = document.getElementById("s");
+    const inpVekt = (document.getElementById("vekt"));
+    const btnberegn = document.getElementById("beregn");
+    btnberegn.addEventListener("click", beregnVansker);
 
+    function beregnVansker() {
+        divS.className = "soyle";
+        // @ts-ignore
+        const vekt = inpVekt.valueAsNumber;
+        if (vekt < 0 || vekt > 250) {
+            alert("Skriv en rimelig vekt!");
+            return;
+        }
+        if (vekt < 8) {
+            divS.innerHTML = "lett";
+            divS.classList.add("lett");
+        } else if (vekt < 16) {
+            divS.innerHTML = "middels";
+            divS.classList.add("middels");
+        } else {
+            divS.innerHTML = "tung";
+            divS.classList.add("tung");
+        }
+    }
 
-    classList.add("fffg ")
-    classList.name =
 }
